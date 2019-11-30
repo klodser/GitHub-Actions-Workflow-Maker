3afa37a64f5b176803a5dbf08aafb9ea014c50d4
Alin Stefan Olaru <alinstefanolaru@gmail.com>
2019-11-30 11:43:16 +0100

    Added ng-ant

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { registerLocaleData } from '@angular/common';
+// tslint:disable-next-line: match-default-export-name
+import en from '@angular/common/locales/en';
+import { en_US, NgZorroAntdModule, NZ_I18N } from 'ng-zorro-antd';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+registerLocaleData(en);
 @NgModule({
   bootstrap: [AppComponent],
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule, NgZorroAntdModule],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
 })
 export class AppModule {}
